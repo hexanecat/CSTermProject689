a7a65e366e5d13ac2cc7ff4546b3ed84d70c748d
hexanecat <56802331+hexanecat@users.noreply.github.com>
2025-12-02 22:02:09 -0500

    changes2

--- bulk_insert/bulk_insert_staging.py
+++ bulk_insert/bulk_insert_staging.py
@@ -1,118 +1,146 @@
-#this is the bulk insert into staging
-#the goal for this is to put the csvs into objects and then strip out the data/attributes that we need for them from each and prep them
-#for inserts into the staging tables
 from sqlalchemy import create_engine, text
 import pandas as pd
+import os
 
-#read in each file
-hospitalFile = "Hospital_General_Information_updated2.csv"
-hospital_df = pd.read_csv(hospitalFile)
+#basic logging function to serve as a trace for file origin
+def log_results(engine, processName, rowCnt, targetTable, fileName, action_type, message, status):
+    try:
+        with engine.begin() as conn:
+            conn.execute(
+                text("""
+                    INSERT INTO etl.etl_log (
+                        process_name,
+                        target_table,
+                        action_type,
+                        rows_processed,
+                        status,
+                        message,
+                        source_file
+                    ) VALUES (
+                        :process_name,
+                        :target_table,
+                        :action_type,
+                        :rows_processed,
+                        :status,
+                        :message,
+                        :source_file
+                    )
+                """),
+                {
+                    "process_name": processName,
+                    "target_table": targetTable,
+                    "action_type": action_type,
+                    "rows_processed": rowCnt,
+                    "status": status,
+                    "message": message,
+                    "source_file": fileName
+                }
+            )
+    #if logger fails throw hard error
+    except Exception as e:
+        print ("failed to log ETL result: ", e)
+        raise
 
-#income data
-incomeFile = "Income.csv"
-income_df = pd.read_csv(incomeFile, encoding="latin1", on_bad_lines="skip")
+#again we want to do column mappings based on the dynamic value that we set per table so that is why
+#we are not hardcoing anything here
+def get_column_map(engine, dataset_name: str) -> dict:
+    query = """
+        SELECT source_column, target_column
+        FROM map.header_targetcolumn
+        WHERE target_table = %(dataset_name)s
+          AND is_active = TRUE
+    """
+    df_map = pd.read_sql(query, engine, params={"dataset_name": dataset_name})
+    return dict(zip(df_map["source_column"], df_map["target_column"]))
+
+#simple truncate table function for truncating all of the staging tables that raw data gets ingested into
+#this should be done at the start of every pipeline that needs to be triggered
+def truncate_staging_tables(engine):
+    try:
+        with engine.begin() as conn:
+            # truncate tables dynamically via truncate sproc
+            conn.execute(text("SELECT truncate_staging_tables();"))
+    except Exception as e:
+        print(f"Error: {e}")
+
+#function to insert data into staging
+def insert_data_into_staging(engine, df, targetTable, fileName, processName, targetSchema):
+    processName = "insert " + fileName + " into " + targetTable
+    rowCnt = 0
+    try:
+        with engine.begin() as conn:
+            df.to_sql(
+                name= targetTable,
+                schema=targetSchema,
+                con=conn,
+                if_exists="append",
+                index=False,
+            )
+            rowCnt = len(df)
+            #this will be a success log
+            message = 'rows inserted into staging successfully'
+            log_results(engine,processName,rowCnt, targetTable, fileName, 'I',message, 'success')
+
+    #if there is an error log then log it here
+    except Exception as e:
+        error_msg = str(e)
+        log_results(engine,processName,rowCnt, targetTable, fileName, 'E', error_msg, 'error')
 
 
-#
-#do all the extracting of attributes here to prep for the staging insert
-#we need to extractand rename columns to match to staging.hospital
-#then we need to do the same for staging.county
-#maybe make the map table something in the database and if we ever need to read from it we can just pull straight from there
-#and have it all be dynamic
-#
-hospital_column_map = {
-    "Facility ID": "facility_id",
-    "Facility Name": "facility_name",
-    "Address": "address",
-    "City/Town": "city_town",
-    "State": "state_code",
-    "ZIP Code": "zip_code",
-    "County/Parish": "county_parish",
-    "Telephone Number": "telephone_number",
-    "Hospital Type": "hospital_type",
-    "Hospital Ownership": "hospital_ownership",
-    "Emergency Services": "emergency_services",
-    # adjust this if your maternal health column is named differently
-    "Meets criteria for birthing friendly designation": "birthing_friendly_designation",
-    "Hospital overall rating": "hospital_overall_rating",
-}
 
-#after this we need to rename the columns that we care about for the dataframe that we are doing an insert into
-hospital_stage_df = hospital_df[list(hospital_column_map.keys())].rename(
-    columns=hospital_column_map
-)
+def main():
+    #read in each file both the hospital data and the income data from the csvs we have generated
+    hospitalFile = "mockdata/Hospital_General_Information_updated2.csv"
+    hospitalFileName = os.path.basename(hospitalFile)
+    hospital_df = pd.read_csv(hospitalFile)
 
-#
-#now do the bulk insert into staging
-#
+    countyFile = "mockdata/Income.csv"
+    countyFileName = os.path.basename(countyFile)
+    county_df = pd.read_csv(countyFile, encoding="latin1", on_bad_lines="skip")
 
-county_column_map = {
-    "FIPS": "fips_code",
-    "State": "state_code",
-    "County": "county_name",
-    "Attribute": "attribute",
-    "Value": "value",
-}
 
-county_stage_df = income_df[list(county_column_map.keys())].rename(
-    columns=county_column_map
-)
+    #make engine to be able to connect to postgres db
+    #make svcAccount so i can query the db using my other acct
+    username = "svcAccount"
+    password = "ibettergetanA"
+    host = "localhost"
+    port = "5432"
+    database = "TermProject"
 
-#make engine 
-username = "postgres"
-password = "frankocean"
-host = "localhost"
-port = "5432"
-database = "TermProject"
+    #build engine to be able to connect to the postgres database
+    engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}')
+    print("Engine URL:", engine.url)
 
-#build engine to be able to connect to the postgres database
-engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}')
-print("Engine URL:", engine.url)
+    #dynamically get column values here
+    hospital_column_map = get_column_map(engine, "hospital")
 
-#since these are staging tables, we will want to do truncate and reload on all of them 
-with engine.begin() as conn:
-    # clear out old data for a clean reload
-    conn.execute(text("TRUNCATE TABLE staging.hospital_dim;"))
-    conn.execute(text("TRUNCATE TABLE staging.county_dim;"))
+    #we only want to keep/rename the columns that we need inserted into the dataframe for both
+    hospital_stage_df = hospital_df[list(hospital_column_map.keys())] \
+        .rename(columns=hospital_column_map)
 
-    # insert into staging.hospital
-    hospital_stage_df.to_sql(
-        name="hospital_dim",
-        schema="staging",
-        con=conn,
-        if_exists="append",
-        index=False,
-    )
+    county_column_map = get_column_map(engine, "county")
+    county_stage_df = county_df[list(county_column_map.keys())] \
+        .rename(columns=county_column_map)
 
-    # insert into staging.county_dim
-    county_stage_df.to_sql(
-        name="county_dim",
-        schema="staging",
-        con=conn,
-        if_exists="append",
-        index=False,
-    )
+    #since these are staging tables, we will want to do truncate and reload on all of them 
+    try:    
+        truncate_staging_tables(engine)
+    except Exception as e:
+        #cast the error message as a string so we can put it in the db
+        error_msg = str(e)
 
-print("Staging loads complete:")
-print(f"  staging.hospital rows:   {len(hospital_stage_df)}")
-print(f"  staging.county_dim rows: {len(county_stage_df)}")
+    #once tables are truncated continue on to the next step
+    #start inserting data into the staging table
+    try:    
+        insert_data_into_staging(engine, hospital_stage_df, 'hospital', hospitalFileName, 'insert into staging','staging')
+        insert_data_into_staging(engine, county_stage_df, 'county', countyFileName, 'insert into dock','dock')
+    except Exception as e:
+        #cast the error message as
+        error_msg = str(e)
 
-#after the staging comppletes we will want to trigger the stored procedures for scrubbing the data.
-#rows inserted into staging now we want to trigger the scrub
-#then after the scrub we want to trigger loading into the dims 
-#then after that insert into the fact tables
+    #when we are done using the engine at the end just dispose and get rid of it
+    finally:
+        engine.dispose()
 
-# with engine.connect() as conn:
-#     #we need to run our scrubs that just got the inserts from staging
-#     conn.execute(text("select staging.scrub_county_staging();"))
-#     conn.execute(text("select staging.scrub_hospital_staging();"))
-
-#     #then based off the scrubbed staging data we need to loaded the dim tables
-#     conn.execute(text("select dbo.load_county_dim_from_staging();"))
-#     conn.execute(text("select dbo.load_hospital_dim_from_staging();"))
-
-#     #then finally we need to load the access fact table and right now datedim just one cause i only have one val there
-#     #and maybe i will make this different not sure yet
-#     conn.execute(text("select dbo.load_hospital_access_fact(1);"))
-
-#     conn.commit()
+if __name__ == "__main__":
+    main()
